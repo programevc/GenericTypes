2556ceafa81a56a28db3f6e28a9cee376ae092fd
programevc <programevc@gmail.com>
2020-07-29 16:14:51 -0300

    fixed example

--- TiposGenericos/Program.cs
+++ TiposGenericos/Program.cs
@@ -7,16 +7,16 @@ namespace TiposGenericos
     {
         static void Main(string[] args)
         {
-            //var lista2 = new GenericClassTypes<PedidoEntity>();
-            //lista2.MinhaLista.Add(new PedidoEntity() { Valor = 100});
-            //lista2.MinhaLista.Add(new PedidoEntity() { Valor = 322 });
+            var lista2 = new GenericClassTypes<PedidoEntity>();
+            lista2.MinhaLista.Add(new PedidoEntity() { Valor = 100 });
+            lista2.MinhaLista.Add(new PedidoEntity() { Valor = 322 });
 
             var Pedido = new PedidoEntity();
             GenericMetodoTypes.GeraId(Pedido);
 
             var outra = new OutraEntity();
             outra.Id = Guid.NewGuid();
-            var datas = GenericMetodoTypes.GetcamposData(Pedido);
+            var datas = GenericMetodoTypes.GetcamposData(outra);
 
 
             Console.ReadKey();
